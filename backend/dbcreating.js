28159bd205b1214874a4dce0c7bd574644c0f00f
focusjk <jiraphat.khupanit@gmail.com>
2020-02-03 17:00:10 +0700

    remove dbconnection

--- backend/dbcreating.js
+++ backend/dbcreating.js
@@ -1,18 +1,18 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-});
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root',
+// });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log('Connected!');
-  connection.query('CREATE DATABASE cucarpool', function(err, result) {
-    if (err) throw err;
-    console.log('Database created');
-    connection.end();
-  });
-});
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   console.log('Connected!');
+//   connection.query('CREATE DATABASE cucarpool', function(err, result) {
+//     if (err) throw err;
+//     console.log('Database created');
+//     connection.end();
+//   });
+// });
 
-module.exports = connection;
+// module.exports = connection;
