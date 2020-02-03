3e95676418ff45445795f6d2fcdd68da6a39038f
focusjk <jiraphat.khupanit@gmail.com>
2020-02-03 17:05:26 +0700

    gitignore

--- backend/dbconnection.js
+++ backend/dbconnection.js
@@ -1,11 +1,11 @@
-// var mysql = require('mysql');
-// var connection = mysql.createConnection({
-//   host: 'localhost',
-//   port: '3306',
-//   user: 'root',
-//   password: 'root',
-//   database: 'cu-carpool',
-//   insecureAuth: true,
-// });
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: '3306',
+  user: 'root',
+  password: 'root',
+  database: 'cu-carpool',
+  insecureAuth: true,
+});
 
-// module.exports = connection;
+module.exports = connection;
--- backend/dbschemacreating.js
+++ backend/dbschemacreating.js
@@ -1,20 +1,20 @@
-// var mysql = require('mysql');
-// var query = require('./sql.js');
+var mysql = require('mysql');
+var query = require('./sql.js');
 
-// var connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'root',
-//   database: 'cucarpool',
-//   multipleStatements: true,
-// });
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'cucarpool',
+  multipleStatements: true,
+});
 
-// connection.connect(function(err) {
-//   if (err) throw err;
-//   console.log('Connected!');
-//   connection.query(query, function(err, result) {
-//     if (err) throw err;
-//     console.log('Table created');
-//     connection.end();
-//   });
-// });
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected!');
+  connection.query(query, function(err, result) {
+    if (err) throw err;
+    console.log('Table created');
+    connection.end();
+  });
+});
