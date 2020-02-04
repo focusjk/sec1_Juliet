78492d092c0cee0c171607b67910f50e0e1ff598
maan-mork <ajinn@mann-mork.local>
2020-02-05 01:34:23 +0700

    .

--- backend/dbschemacreating.js
+++ backend/dbschemacreating.js
@@ -4,7 +4,7 @@ var query = require('./sql.js');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'root',
+  password: 'admin',
   database: 'cucarpool',
   multipleStatements: true,
 });
--- backend/service/admin.js
+++ backend/service/admin.js
@@ -5,4 +5,4 @@ const login = (username, password) => {
   );
 };
 
-export { login };
+module.exports =  { login };
