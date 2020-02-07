a9c1c0f8bb2dce0623a088dfc4cd7e6ac7b0defa
j <facebookhacker15@hotmail.com>
2020-02-07 13:54:43 +0700

    initiate driver request page

--- cu-carpool/src/page/DriverRequest.js
+++ cu-carpool/src/page/DriverRequest.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import profile from '../profile.jpg';
 
 class ProfileLog extends React.Component {
   render() {
-    return <div>ProfileLog</div>;
+    return (
+     <Grid container direction="column" justify="flex-start">
+    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '20vh'}}>
+    <h1> Driver's form </h1>
+    </div>
+     <img
+            src={profile}
+            height={80}
+	    width={80}
+	    style={{ alignSelf: 'center',borderRadius: 400/ 2 }}
+          />
+     </Grid>
+     )
   }
 }
 
