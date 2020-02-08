95dd2fa6659eba2b26594d4ee988baecfec7da28
unknown <rbbrki@gmail.com>
2020-02-08 18:25:34 +0700

    edit after remove driver table

--- backend/service/admin.js
+++ backend/service/admin.js
@@ -10,27 +10,9 @@ const login = (username, password) => {
 // YIN
 const getAllMember = (callback) => {
   console.log('get all member')
-  return db.query(`SELECT 
-            m.id as id,
-            m.username as username,
-            m.firstname as firstname,
-            m.lastname as lastname,
-            m.phone_number as phone_number,
-            m.email as email,
-            m.photo as photo,
-            m.driver_status as driver_status,
-            d.driving_license as driving_license,
-            d.approved_at as approved_at,
-            d.approved_by as approved_by,
-            d.rejected_at as rejected_at,
-            d.rejected_by as rejected_by,
-            d.edited_at as edited_at,
-            d.driver_status as driver_status,
-            d.car_brand as car_brand,
-            d.plate_license as plate_license,
-            d.capacity as capacity
-        FROM members as m left join driver as d ON m.id = d.id`
-        , callback);
+  return db.query(`SELECT id,username,firstname,lastname,phone_number,email,photo,driver_status,driving_license,
+                  approved_at, approved_by,rejected_at,rejected_by,edited_at, driver_status
+                  FROM members WHERE driver_status = 'approved'` , callback);
 }
 
 
