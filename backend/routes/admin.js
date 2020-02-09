14d568aad337dc67b8de90d9092896ba7bd5c6fc
littlemill <romnalink.@romnalins-MacBook-Pro.local>
2020-02-09 15:10:11 +0700

    make it possible to add to the database

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -9,8 +9,17 @@ router.post('/login', function(req, res, next) {
   res.send({ success: true, user: data });
 });
 
+// YIN
 router.get('/driver', function(req, res, next) {
-  res.send('respond with a resource');
+  adminService.getAllMember((err, result) => {
+    if (err) {
+      res.json({success: false, error: err, message: "Cannot access drivers information"});
+    }
+    else {
+        console.log(result)
+        res.json({ success: true, driver:[ ...result ]});
+    }
+  })
 });
 
 router.post('/driver-approve', function(req, res, next) {
--- backend/routes/trip.js
+++ backend/routes/trip.js
@@ -2,21 +2,24 @@ var express = require('express');
 var router = express.Router();
 var tripService = require('../service/trip');
 
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.post('/create', function(req, res, next) {
   // dateTime format "YYYY-MM-DD hh:mm:ss"
-  var now = new Date();
-  var date = now.toISOString().split('T')[0];
-  var time = now.toLocaleTimeString();
-  now = date + ' ' + time;
-  console.log('created_time :', now);
-  tripService.createTrip(now, req.body, (err, result) => {
+  var created_at = new Date();
+  var date = created_at.toISOString().split('T')[0];
+  var time = created_at.getHours() + ':' + created_at.getMinutes() + ':' + created_at.getSeconds();
+  // var time = now.toLocaleTimeString();
+  created_at = date + ' ' + time;
+  console.log('created_at :', created_at);
+  tripService.createTrip(created_at, req.body, (err, result) => {
     if (err) {
-      res.json(err);
+      console.log(err);
+      res.json({ success: false, error: err.sqlMessage, message: 'CANNOT CREATE TRIP!!!' });
     } else {
+      console.log('----------------create Trip----------------');
       console.log(result);
       res.json({ success: true, id: result.insertId });
     }
