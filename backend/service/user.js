12c1a540efd24e8054b009c3a80061205b8208f1
littlemill <romnalink.@romnalins-MacBook-Pro.local>
2020-02-09 14:52:53 +0700

    make it possible to add to the database

--- backend/service/user.js
+++ backend/service/user.js
@@ -2,7 +2,8 @@ var db = require('../dbconnection');
 const register = (username, password,firstname,lastname,phone_number,email,photo,created_at,amount) => {
     console.log('add all data for user = ',username);
     return db.query(
-      `INSERT INTO members (username,password,firstname,lastname,phone_number,email,photo,created_at,amount) VALUES (?,?,?,?,?,?,?,?,?)`,[username,password,firstname,lastname,phone_number,email,photo,created_at,amount]
+      `INSERT INTO members (username,password,firstname,lastname,phone_number,email,photo,created_at,amount) VALUES (?,?,?,?,?,?,?,?,?)`
+      ,[username,password,firstname,lastname,phone_number,email,photo,created_at,amount]
     );
   };
   
