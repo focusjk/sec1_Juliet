2a40a9c0645fa647a5b47eca2bbc1528d5f46ca5
littlemill <romnalink.@romnalins-MacBook-Pro.local>
2020-02-10 18:15:08 +0700

    add id

--- backend/service/user.js
+++ backend/service/user.js
@@ -10,7 +10,7 @@ const register = (username,data,created_at,amount,callback) => {
 
   const getMemberInfo = (username,callback) => {
     console.log('get member info')
-    return db.query(`SELECT username,firstname,lastname,phone_number,email,photo,driver_status,card_holder_name,card_number,
+    return db.query(`SELECT id,username,firstname,lastname,phone_number,email,photo,driver_status,card_holder_name,card_number,
       card_code,card_expiry_date,amount FROM members WHERE username = ?`, [username],callback);
   }
   
