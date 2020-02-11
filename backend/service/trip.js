dcb50f5d800f66f21be8f70c5497a8bfe68d0b5f
focusjk <jiraphat.khupanit@gmail.com>
2020-02-11 23:41:41 +0700

    edit search algo

--- backend/service/trip.js
+++ backend/service/trip.js
@@ -22,11 +22,11 @@ const createTrip = (
 ) => {
   return db.query(
     `INSERT INTO trip ` +
-      `(departure_latitude,departure_longtitude,departure_detail,
+    `(departure_latitude,departure_longtitude,departure_detail,
                                         destination_latitude,destination_longtitude,destination_detail,
                                         start_datetime,owner,car_brand,plate_license,capacity,created_at,
                                         departure_province,destination_province,price)` +
-      `VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
+    `VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
     [
       departure_latitude,
       departure_longtitude,
@@ -48,7 +48,7 @@ const createTrip = (
   );
 };
 
-const searchTrip = ({ departure, destination, selectedDate },callback)=>{
+const searchTrip = ({ departure, destination, selectedDate }, callback) => {
   return db.query(`SELECT 
                           trip.id,
                           trip.departure_detail,
@@ -60,11 +60,13 @@ const searchTrip = ({ departure, destination, selectedDate },callback)=>{
                           Count(Distinct request.id) AS request,
                           status 
                           FROM trip left join request on trip.id = request.trip_id AND request_status = "approved"
-                          WHERE trip.start_datetime LIKE '%` + selectedDate + `%' AND
-                          ( trip.departure_province LIKE '%` + departure + `%' OR
-                          trip.destination_province LIKE '%` + destination + `%' ) AND
+                          WHERE trip.start_datetime LIKE '%` + selectedDate + `%' AND (
+                          trip.departure_province ="`+ departure + `"OR
+                          ( trip.departure_province LIKE '%` + departure + `%' AND
+                          trip.destination_province LIKE '%` + destination + `%' ) OR
+                          trip.destination_province ="` + destination + `")AND
                           trip.status = "opening"
                           GROUP BY trip.id
-                          ORDER BY trip.start_datetime`,callback);
+                          ORDER BY trip.start_datetime`, callback);
 };
-module.exports = { createTrip , searchTrip};
+module.exports = { createTrip, searchTrip };
