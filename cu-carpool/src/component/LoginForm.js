c73fd1d535e516096019dc64b7aa6b7e38229f5b
maan-mork <naon123@hotmail.com>
2020-02-11 13:08:12 +0700

    integrate front-back : login admin page

--- cu-carpool/src/component/LoginForm.js
+++ cu-carpool/src/component/LoginForm.js
@@ -1,37 +1,47 @@
-import React from 'react';
-import { Input, Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import IconButton from '@material-ui/core/IconButton';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import React from "react";
+import { Input, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import IconButton from "@material-ui/core/IconButton";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
 // import { withRouter } from 'react-router-dom';
-
-
+import { MyFullWidthButton } from "./MyButton";
 const useStyles = makeStyles({
   root: {
-    color: '#777777',
-    marginBottom: 20,
-  },
+    color: "#777777",
+    marginBottom: 20
+  }
 });
 
-const LoginForm = () => {
+const LoginForm = ({
+  username,
+  password,
+  setUsername,
+  setPassword,
+  handleLogin,
+  error
+}) => {
   const classes = useStyles();
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  // const [username, setUsername] = React.useState('');
+  // const [password, setPassword] = React.useState('');
   const [showPassword, setShowPassword] = React.useState(false);
   return (
     <Grid container direction="column" justify="flex-start">
       <div style={{ marginBottom: 5 }}> USERNAME </div>
-      <Input placeholder="Username" className={classes.root} 
+      <Input
+        placeholder="Username"
+        className={classes.root}
         value={username}
         onChange={e => setUsername(e.target.value)}
       />
 
       <div style={{ marginBottom: 5 }}> PASSWORD </div>
-      <Input placeholder="Password" className={classes.root}
+      <Input
+        placeholder="Password"
+        className={classes.root}
         id="standard-adornment-password"
-        type={showPassword ? 'text' : 'password'}
+        type={showPassword ? "text" : "password"}
         value={password}
         onChange={e => setPassword(e.target.value)}
         endAdornment={
@@ -39,7 +49,7 @@ const LoginForm = () => {
             <IconButton
               aria-label="toggle password visibility"
               onClick={() => {
-                setShowPassword( !showPassword );
+                setShowPassword(!showPassword);
               }}
             >
               {showPassword ? <Visibility /> : <VisibilityOff />}
@@ -47,7 +57,15 @@ const LoginForm = () => {
           </InputAdornment>
         }
       />
-
+      <MyFullWidthButton
+        style={{ marginTop: 40 }}
+        onClick={() => {
+          handleLogin();
+        }}
+      >
+        Sign In
+      </MyFullWidthButton>
+      {error !== "" && <div style={{ color: "red" }}>{error}</div>}
     </Grid>
   );
 };
