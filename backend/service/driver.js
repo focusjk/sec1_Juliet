516c993e8e37a1f89d84a6c6a76b1fca62374cf0
unknown <rbbrki@gmail.com>
2020-02-15 18:36:41 +0700

    set approve/reject to null

--- backend/service/driver.js
+++ backend/service/driver.js
@@ -5,7 +5,9 @@ const getByID = (ID, callback) => {
 }
 
 const driverReq = (ID, data, callback) => {
-    return db.query(`UPDATE members SET driver_status = 'pending' , ? WHERE id = ?`, [data, ID], callback);
+    return db.query(`UPDATE members SET driver_status = 'pending',approved_at = null,
+                    approved_by = null,rejected_at = null,
+                    rejected_by = null , ? WHERE id = ?`, [data, ID], callback);
 }
 
 module.exports = { getByID, driverReq };
