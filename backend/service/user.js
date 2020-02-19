1550017c88d826aeae00439856b1bedc28870c48
littlemill <millmill.romnalin@hotmail.com>
2020-02-19 20:26:42 +0700

    credit-card-related fields are already removed or commented.

--- backend/service/user.js
+++ backend/service/user.js
@@ -8,20 +8,19 @@ const register = (username, data, created_at, amount, callback) => {
 };
 
 const getMemberInfo = (username, callback) => {
-  return db.query(`SELECT id,username,firstname,lastname,phone_number,email,photo,driver_status,card_holder_name,card_number,
-      card_code,card_expiry_date,amount,driving_license,approved_at,approved_by,rejected_at,rejected_by,edited_at FROM members WHERE username = ?`, [username], callback);
+  return db.query(`SELECT id,username,firstname,lastname,phone_number,email,photo,driver_status,
+      amount,driving_license,approved_at,approved_by,rejected_at,rejected_by,edited_at FROM members WHERE username = ?`, [username], callback); //credit-card-related fields are removed from this line
 }
 
 function login(username, password, callback) {
-  return db.query(`SELECT id,username,firstname,lastname,phone_number,email,photo,driver_status,card_holder_name,card_number,
-    card_code,card_expiry_date,amount,driving_license,approved_at,approved_by,rejected_at,rejected_by,edited_at FROM members WHERE username = ? AND password = ?`, [username, password], callback);
+  return db.query(`SELECT id,username,firstname,lastname,phone_number,email,photo,driver_status,
+    amount,driving_license,approved_at,approved_by,rejected_at,rejected_by,edited_at FROM members WHERE username = ? AND password = ?`, [username, password], callback); //credit-card-related fields are removed from this line
 
 }
 
 const editMemberInfo = (id, body, callback) => {
-  const { firstname, lastname, phone_number, email, photo, card_holder_name, card_number, card_code, card_expiry_date } = body; 
+  const { firstname, lastname, phone_number, email, photo} = body; //credit-card-related fields are removed from this line
     return db.query(`UPDATE members SET firstname = ?,lastname = ? ,phone_number = ?,email = ?,photo = ?,
-      card_holder_name = ?,card_number = ?,card_code = ?,card_expiry_date = ? WHERE id = ?;`
-      , [firstname, lastname, phone_number, email, photo, card_holder_name, card_number, card_code, card_expiry_date, id], callback); 
-}
+      WHERE id = ?;` //credit-card-related fields are removed from this line
+      , [firstname, lastname, phone_number, email, photo id], callback); //credit-card-related fields are removed from this line
 module.exports = { register, getMemberInfo, login, editMemberInfo };
