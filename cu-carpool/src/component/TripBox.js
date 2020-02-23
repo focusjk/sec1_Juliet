0e85d275caee9e0a13ec9b332b82b14129c8fbd9
j <facebookhacker15@hotmail.com>
2020-02-23 20:52:26 +0700

    minor correction

--- cu-carpool/src/component/TripBox.js
+++ cu-carpool/src/component/TripBox.js
@@ -78,7 +78,7 @@ const TripBox = ({ history, data }) => {
             <div style={{ fontSize: "20px" }}>200 ฿</div>
             <MyLink goto="/trip-request">see request</MyLink>
             <MyLink goto="/TripMemberforDriver">see trip member</MyLink>
-	  <MyLink goto="/TripMemberforMember">see trip member</MyLink>
+
 
           </div>
         </div>
