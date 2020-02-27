0ca3adad52802678cc0de91935813e660e7a4780
focusjk <jiraphat.khupanit@gmail.com>
2020-02-27 09:36:47 +0700

    edit report

--- cu-carpool/src/page/AdminReport.js
+++ cu-carpool/src/page/AdminReport.js
@@ -31,21 +31,19 @@ const MyToggleButton = withStyles({
 
 class AdminReport extends React.Component {
 
-  state = { list: [], mode: 0 }
+  state = { list: [], filteredList: [], mode: 0 }
   componentDidMount() {
     this.fetchData()
   }
   fetchData = async () => {
     const response = await axios.get("http://localhost:4000/admin/report");
     const { success, report } = response.data
-    console.log(response.data)
     if (success) {
       this.setState({ list: report })
     }
+    this.filter(this.state.mode)
   }
-  handleRead = async (id,is_read) => {
-    const { username } = this.props.user;
-    console.log({ id, admin_name: username })
+  handleRead = async (id, is_read) => {
     const response = await axios.post("http://localhost:4000/admin/report/read", { id, is_read: !is_read });
     const { success } = response.data
     if (success) {
@@ -53,57 +51,75 @@ class AdminReport extends React.Component {
     }
   }
 
+  handleMode = (e, value) => {
+    if (value !== null) {
+      this.setState({ mode: value })
+      this.filter(value)
+    }
+  }
 
+  filter = mode => {
+    const { list } = this.state
+    let filteredList = []
+    if (mode == 0) {
+      filteredList = list
+    } else if (mode == 1) {
+      filteredList = list.filter(({ is_read }) => is_read == 1)
+    } else {
+      filteredList = list.filter(({ is_read }) => is_read == 0)
+    }
+    this.setState({ filteredList })
+  }
   render() {
     return (
 
       <Grid container direction="column" justify="center" style={{ width: "100%" }}>
-      <MyHeader style={{ justifyContent: 'left' }}>
-        > Report Log
+        <MyHeader style={{ justifyContent: 'left' }}>
+          > Report Log
       </MyHeader>
 
-      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '40 px'}}>
-      <ToggleButtonGroup value={this.state.mode}>
-        <MyToggleButton style = {{width: '140px', height: '42px', border: "1px solid #C78899",}} value={0}> All </MyToggleButton>
-        <MyToggleButton style = {{width: '140px', height: '42px', border: "1px solid #C78899",}} value={1}> Read </MyToggleButton>
-        <MyToggleButton style = {{width: '140px', height: '42px', border: "1px solid #C78899",}} value={2}> Unread </MyToggleButton>
-      </ToggleButtonGroup>
-      </div>
-  
-      <div style={{ margin: "32px 0", alignSelf: 'center' }}>
-         <EmptyBox data={this.state.list} />
-         {this.state.list.map(({id, member_id, username, firstname, lastname, topic, comment,photo, created_at, is_read }) =>
-          <div style={{ maxWidth: 740, display: 'flex', flexDirection: 'column', border: "1px solid #C4C4C4", padding: '16px 48px', marginBottom: '16px'}}>
-            <div style={{ display: 'flex', alignItems: 'center', marginRight: '16px' }}>
-              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center',flexWrap: 'wrap', width: '150px', marginRight: '16px' }}>
-                 <img
-                  src={photo}
-                  height={100}
-                  width={100}
-                  style={{ alignItems: 'center', borderRadius: "100%", marginBottom: "16px" }}
-                />
-                <div style={{ alignSelf: 'center' }}> {username} </div>
-              </div>
-              <div style={{ maxWidth: 407, display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-                <div> <b> Member ID:</b> {member_id}</div>
-                <div> <b>Name:</b> {firstname} {lastname}</div>
-                <div> <b>Topic:</b> {topic}</div>
-                <div> <b>Comment:</b> </div>
-                <div style={{  wordBreak: "break-word", textIndent: "50px"}} >  {comment} </div>
-                <div> <b>Created at :</b> {created_at} </div>
-              </div>
-              </div>
-            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '16px'}}>
-                {!!is_read && <MyGreyButton onClick={()=> this.handleRead(id,is_read)}>Read</MyGreyButton>}
-                {!is_read && <MyButton onClick={()=> this.handleRead(id,is_read)}>Read</MyButton>}
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '40 px' }}>
+          <ToggleButtonGroup exclusive value={this.state.mode} onChange={this.handleMode}>
+            <MyToggleButton style={{ width: '140px', height: '42px', border: "1px solid #C78899", }} value={0}> All </MyToggleButton>
+            <MyToggleButton style={{ width: '140px', height: '42px', border: "1px solid #C78899", }} value={1}> Read </MyToggleButton>
+            <MyToggleButton style={{ width: '140px', height: '42px', border: "1px solid #C78899", }} value={2}> Unread </MyToggleButton>
+          </ToggleButtonGroup>
+        </div>
 
+        <div style={{ margin: "32px 0", alignSelf: 'center' }}>
+          <EmptyBox data={this.state.filteredList} />
+          {this.state.filteredList.map(({ id, member_id, username, firstname, lastname, topic, comment, photo, created_at, is_read }) =>
+            <div style={{ maxWidth: 740, display: 'flex', flexDirection: 'column', border: "1px solid #C4C4C4", padding: '16px 48px', marginBottom: '16px' }}>
+              <div style={{ display: 'flex', alignItems: 'center', marginRight: '16px' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', flexWrap: 'wrap', width: '150px', marginRight: '16px' }}>
+                  <img
+                    src={photo}
+                    height={100}
+                    width={100}
+                    style={{ alignItems: 'center', borderRadius: "100%", marginBottom: "16px" }}
+                  />
+                  <div style={{ alignSelf: 'center' }}> {username} </div>
+                </div>
+                <div style={{ maxWidth: 407, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                  <div> <b> Member ID:</b> {member_id}</div>
+                  <div> <b>Name:</b> {firstname} {lastname}</div>
+                  <div> <b>Topic:</b> {topic}</div>
+                  <div> <b>Comment:</b> </div>
+                  <div style={{ wordBreak: "break-word", textIndent: "50px" }} >  {comment} </div>
+                  <div> <b>Created at:</b> {created_at} </div>
+                </div>
+              </div>
+              <div style={{ display: 'flex', justifyContent: 'center', marginTop: '16px' }}>
+                {!!is_read && <MyGreyButton onClick={() => this.handleRead(id, is_read)}>Unread</MyGreyButton>}
+                {!is_read && <MyButton onClick={() => this.handleRead(id, is_read)}>Read</MyButton>}
+
+              </div>
             </div>
-          </div>
-           )}
-      </div>
-    </Grid>
-     )
-     }
+          )}
+        </div>
+      </Grid>
+    )
+  }
 }
 
 
