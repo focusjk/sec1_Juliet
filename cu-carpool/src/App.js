35f2b04fd33a4848d3d245cac651c91be3b302b4
unknown <rbbrki@gmail.com>
2020-03-08 16:51:32 +0700

    edit app

--- cu-carpool/src/App.js
+++ cu-carpool/src/App.js
@@ -53,7 +53,7 @@ const useStyles = makeStyles({
 });
 const App = () => {
   //for member
-   const [user, setUser] = React.useState({ id: 1, driver_status: "approved" });
+    const [user, setUser] = React.useState({ id: 1, driver_status: "approved" });
   // for admin
   //  const [user, setUser] = React.useState({ username: 'admin' });
   // for other
