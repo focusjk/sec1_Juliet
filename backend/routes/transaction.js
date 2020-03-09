e3820c65863bdf34daf07387bf49c1593267723e
littlemill <millmill.romnalin@hotmail.com>
2020-03-09 23:23:05 +0700

    transaction commented

--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router = express.Router();
-var transactionService = require('../service/transaction');
-var util = require('../util');
+// var express = require('express');
+// var router = express.Router();
+// var transactionService = require('../service/transaction');
+// var util = require('../util');
 
 
-module.exports = router;
+// module.exports = router;
