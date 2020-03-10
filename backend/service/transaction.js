477263f0e5b38d35705aef593af8a12396ac0d87
littlemill <millmill.romnalin@hotmail.com>
2020-03-10 11:53:39 +0700

    transaction bug fixed

--- backend/service/transaction.js
+++ backend/service/transaction.js
@@ -8,13 +8,14 @@ const createTransaction = (amount,member_id,created_at,type,callback) => {
 const refundTransaction = async (request_id,trip_id,created_at,callback) => {
     const price = await util.promisifyQuery(`SELECT trip.price FROM trip WHERE id = ?`, [trip_id]);
     const member = await util.promisifyQuery(`SELECT request.member_id FROM request where id = ?`, [request_id]);
-    const {price:amount} = price[0];
+    const {price:trip_price} = price[0];
     const {member_id} = member[0];
     const type = 3;
-    createTransaction(amount,member_id,created_at,type,callback);
+    createTransaction(trip_price,member_id,created_at,type,callback);
     const wallet_amount = await util.promisifyQuery(`SELECT members.amount FROM members WHERE members.id = ?`,[member_id]);
     const {amount:member_wallet_amount} = wallet_amount[0];
-    const updated_amount = price+member_wallet_amount;
+    console.log('m =',member_wallet_amount);
+    const updated_amount = trip_price+member_wallet_amount;
     return db.query(`UPDATE members SET members.amount = ? WHERE members.id = ?`,[updated_amount,member_id],callback);
 }
 
