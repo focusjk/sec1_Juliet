0b939bbbd8c3468e63644a9565796ed672401b3f
focusjk <jiraphat.khupanit@gmail.com>
2020-03-10 14:29:37 +0700

    edit name

--- backend/routes/trip.js
+++ backend/routes/trip.js
@@ -135,10 +135,10 @@ router.post('/getInTheCar', (req, res, next) => {
   })
 });
 
-router.post('/cancelTrip', (req, res, next) => {
+router.post('/cancelRequest', (req, res, next) => {
   const { id: request_id } = req.body;
   const cancel_time = util.timeformatter(new Date());
-  tripService.cancelTrip(request_id,cancel_time, (err, result) => {
+  tripService.cancelRequest(request_id, cancel_time, (err, result) => {
     if (err) {
       res.json({ success: false, message: 'Cannot cancel your trip' });
     } else {
