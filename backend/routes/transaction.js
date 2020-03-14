f847e9f0cdf8a39eeeb86234f5d649e1cb4eb8a8
maan-mork <naon123@hotmail.com>
2020-03-14 20:24:45 +0700

    back for user see own transaction log

--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -1,7 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var transactionService = require('../service/transaction');
-var util = require('../util');
+var transactionService = require("../service/transaction");
+var util = require("../util");
 
+router.get("/log", (req, res, next) => {
+  const { member_id } = req.query;
+  transactionService.getTransaction(member_id, (err, result) => {
+    if (err) {
+      res.json({
+        success: false,
+        error: err.sqlMessage,
+        message: "Cannot access database"
+      });
+    } else {
+      res.json({ success: true, transaction: result });
+    }
+  });
+});
 
- module.exports = router;
+module.exports = router;
