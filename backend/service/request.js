6627703cc26ef7e461379f422e700e65267a1e5b
littlemill <millmill.romnalin@hotmail.com>
2020-03-19 01:25:24 +0700

    remove console log ja T-T forgot ngueee

--- backend/service/request.js
+++ backend/service/request.js
@@ -4,7 +4,6 @@ var util = require('../util');
 const createRequest = async (trip_id, member_id, data, created_at, callback) => {
     const check = await util.promisifyQuery(`SELECT count(*) as count FROM request WHERE request.trip_id = ? AND request.member_id = ?`, [trip_id, member_id]);
     const driver = await util.promisifyQuery(`SELECT owner FROM request JOIN trip ON request.trip_id = trip.id WHERE trip_id = ?`,[trip_id]);
-    console.log(driver);
     const {owner} = driver[0];
     const {count} = check[0];
     if (count == 0 && owner != member_id) {
