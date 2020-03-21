e29768529e8464174483317200dd06a95b601059
maan-mork <naon123@hotmail.com>
2020-03-21 17:44:44 +0700

    add backend for get amount for wallet

--- backend/service/user.js
+++ backend/service/user.js
@@ -1,38 +1,70 @@
-var db = require('../dbconnection');
+var db = require("../dbconnection");
 
 const register = (username, data, created_at, amount, callback) => {
   const { password, firstname, lastname, phone_number, email, photo } = data;
   return db.query(
-    `INSERT INTO members (username,password,firstname,lastname,phone_number,email,photo,created_at,amount) VALUES (?,?,?,?,?,?,?,?,?)`
-    , [username, password, firstname, lastname, phone_number, email, photo, created_at, amount], callback
+    `INSERT INTO members (username,password,firstname,lastname,phone_number,email,photo,created_at,amount) VALUES (?,?,?,?,?,?,?,?,?)`,
+    [
+      username,
+      password,
+      firstname,
+      lastname,
+      phone_number,
+      email,
+      photo,
+      created_at,
+      amount
+    ],
+    callback
   );
 };
 
 const getMemberInfo = (username, callback) => {
-  return db.query(`SELECT id,username,firstname,lastname,phone_number,email,photo,driver_status,
-    amount,driving_license,approved_at,approved_by,rejected_at,rejected_by,edited_at,banned_at,banned_by FROM members WHERE username = ?`, [username], callback); //credit-card-related fields are removed from this line
-}
+  return db.query(
+    `SELECT id,username,firstname,lastname,phone_number,email,photo,driver_status,
+    amount,driving_license,approved_at,approved_by,rejected_at,rejected_by,edited_at,banned_at,banned_by FROM members WHERE username = ?`,
+    [username],
+    callback
+  ); //credit-card-related fields are removed from this line
+};
 
 function login(username, password, callback) {
-  return db.query(`SELECT id,username,firstname,lastname,phone_number,email,photo,driver_status,
-    amount,driving_license,approved_at,approved_by,rejected_at,rejected_by,edited_at,banned_at,banned_by FROM members WHERE username = ? AND password = ?`, [username, password], callback); //credit-card-related fields are removed from this line
-
+  return db.query(
+    `SELECT id,username,firstname,lastname,phone_number,email,photo,driver_status,
+    amount,driving_license,approved_at,approved_by,rejected_at,rejected_by,edited_at,banned_at,banned_by FROM members WHERE username = ? AND password = ?`,
+    [username, password],
+    callback
+  ); //credit-card-related fields are removed from this line
 }
 
 const editMemberInfo = (id, body, callback) => {
   const { firstname, lastname, phone_number, email, photo } = body; //credit-card-related fields are removed from this line
-  return db.query(`UPDATE members SET firstname = ?,lastname = ? ,phone_number = ?,email = ?,photo = ?
-      WHERE id = ?;` //credit-card-related fields are removed from this line
-    , [firstname, lastname, phone_number, email, photo, id], callback); //credit-card-related fields are removed from this line
-}
+  return db.query(
+    `UPDATE members SET firstname = ?,lastname = ? ,phone_number = ?,email = ?,photo = ?
+      WHERE id = ?;`, //credit-card-related fields are removed from this line
+    [firstname, lastname, phone_number, email, photo, id],
+    callback
+  ); //credit-card-related fields are removed from this line
+};
 
 const payment = (ID, body, callback) => {
-  const { card_number, card_holder_name, card_expiry_date, card_code, paid_at } = body;
-  return db.query(`UPDATE request SET request_status = 'paid', paid_at = ? WHERE id = ? `, [paid_at, ID], callback);
-}
+  const {
+    card_number,
+    card_holder_name,
+    card_expiry_date,
+    card_code,
+    paid_at
+  } = body;
+  return db.query(
+    `UPDATE request SET request_status = 'paid', paid_at = ? WHERE id = ? `,
+    [paid_at, ID],
+    callback
+  );
+};
 
 const getTripHistory = (member_id, callback) => {
-  return db.query(`SELECT request.id as id,
+  return db.query(
+    `SELECT request.id as id,
                   request.departure_latitude,
                   request.departure_longtitude,
                   request.departure_detail,
@@ -56,8 +88,28 @@ const getTripHistory = (member_id, callback) => {
                   members.firstName as owner_firstname,
                   members.lastName as owner_lastname
                   FROM trip INNER JOIN members ON trip.owner = members.id INNER JOIN request ON request.trip_id = trip.id
-                  WHERE request.member_id = ?`, [member_id], callback);
-}
+                  WHERE request.member_id = ?`,
+    [member_id],
+    callback
+  );
+};
 
-module.exports = { register, getMemberInfo, login, editMemberInfo, payment, getTripHistory };
+const getWallet = (member_id, callback) => {
+  return db.query(
+    `SELECT members.amount
+    FROM members
+    WHERE id = ?`,
+    [member_id],
+    callback
+  );
+};
 
+module.exports = {
+  register,
+  getMemberInfo,
+  login,
+  editMemberInfo,
+  payment,
+  getTripHistory,
+  getWallet
+};
