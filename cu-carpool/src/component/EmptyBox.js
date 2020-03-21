a5710272aa8a8377bcd82ed04c570e6931afe553
maan-mork <naon123@hotmail.com>
2020-03-21 19:33:29 +0700

    check empty object in emptyBox

--- cu-carpool/src/component/EmptyBox.js
+++ cu-carpool/src/component/EmptyBox.js
@@ -1,14 +1,27 @@
-import React from 'react'
-import InboxIcon from '@material-ui/icons/Inbox';
+import React from "react";
+import InboxIcon from "@material-ui/icons/Inbox";
+
+function isEmpty(obj) {
+  return !Object.keys(obj).length > 0;
+}
 
 const EmptyBox = ({ data }) => {
-    if (!!data && data.length === 0) {
-        return <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 48 }}>
-            <InboxIcon color="disabled" style={{ fontSize: 48 }} />
-            <div style={{ color: "#c4c4c4", fontSize: 24 }}>no data</div>
-        </div>
-    } else {
-        return null
-    }
-}
-export default EmptyBox
+  if ((!!data && data.length === 0) || isEmpty(data)) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          margin: 48
+        }}
+      >
+        <InboxIcon color="disabled" style={{ fontSize: 48 }} />
+        <div style={{ color: "#c4c4c4", fontSize: 24 }}>no data</div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
+export default EmptyBox;
