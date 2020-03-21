cc6346889a05aa34e91cdae9f9c824f74eb3a4d1
maan-mork <naon123@hotmail.com>
2020-03-21 20:01:55 +0700

    little edit in transaction Box

--- cu-carpool/src/component/WithdrawalBox.js
+++ cu-carpool/src/component/WithdrawalBox.js
@@ -1,36 +1,56 @@
 import React from "react";
-import Divider from '@material-ui/core/Divider';
+import Divider from "@material-ui/core/Divider";
 
 const WithdrawalBox = ({ data }) => {
-    return (
-        <div>
-            {data.map((x, index) => (
-                <div
-                    key={index}
-                    style={{
-                        border: '1px solid #BDBDBD',
-                        padding: "8px 16px",
-                        flexDirection: "column",
-                        marginBottom: '10px',
-                        fontSize: 16
-                    }}
-                >
-                    <div style={{ display: "flex", alignItems: 'center', justifyContent: "space-between" }}>
-                        <div>Status: {x.status}</div>
-                        <div style={{ fontSize: 20 }}>{x.amount} ฿</div>
-                    </div>
-                    <Divider style={{ margin: '8px 0' }} />
-                    <div style={{ display: "flex", justifyContent: "space-between" }}>
-                        <div>{x.bank_name}</div>
-                        <div>{x.account_number}</div>
-                    </div>
-                    <div>{x.account_name}</div>
-                    <div style={{ fontSize: 14, color: '#BDBDBD' }}>
-                        {x.created_time}
-                    </div>
-                </div>
-            ))}
+  return (
+    <div>
+      {data.map((x, index) => (
+        <div
+          key={index}
+          style={{
+            border: "1px solid #BDBDBD",
+            padding: "8px 16px",
+            flexDirection: "column",
+            marginBottom: "10px",
+            fontSize: 16
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between"
+            }}
+          >
+            <div>Status: {x.status}</div>
+            <div style={{ fontSize: 20 }}>{x.amount} ฿</div>
+          </div>
+          <Divider style={{ margin: "3px 0 8px" }} />
+          <div style={{ display: "flex", justifyContent: "flex-start" }}>
+            <div
+              style={{
+                marginBottom: "1px",
+                display: "flex",
+                flexGrow: 1
+              }}
+            >
+              {x.bank_name}
+            </div>
+            <div
+              style={{
+                marginBottom: "1px",
+                display: "flex",
+                flexGrow: 4
+              }}
+            >
+              {x.account_number}
+            </div>
+          </div>
+          <div style={{ marginBottom: "1px" }}>{x.account_name}</div>
+          <div style={{ fontSize: 14, color: "#BDBDBD" }}>{x.created_time}</div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 export default WithdrawalBox;
