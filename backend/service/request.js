30258063010c414845e0ec9e16d23526c7f85e70
littlemill <millmill.romnalin@hotmail.com>
2020-03-21 19:14:44 +0700

    console.log deleted ngae TT

--- backend/service/request.js
+++ backend/service/request.js
@@ -3,7 +3,6 @@ var util = require('../util');
 
 const createRequest = async (trip_id, member_id, data, created_at, callback) => {
     const check = await util.promisifyQuery(`SELECT count(*) as count FROM request WHERE request.trip_id = ? AND request.member_id = ? AND request.request_status != 'canceled'`, [trip_id, member_id]);
-    console.log(check);
     const driver = await util.promisifyQuery(`SELECT owner FROM request JOIN trip ON request.trip_id = trip.id WHERE trip_id = ?`, [trip_id]);
     const { owner } = driver[0];
     const { count } = check[0];
